7ba2486f7a5ed2984fca2cbdc5d63649d9de6b95
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-13 22:51:39 -0400

    remove unneeded imports

--- setup.py
+++ setup.py
@@ -4,16 +4,12 @@ setup(
     name='bookmark-database',
     description='Python package for interacting with Bookmark\'s database.',
     author='Bookmark Novels',
-    version='1.0.4',
+    version='1.0.5',
     packages=['bookmark_database'],
     url='https://github.com/Bookmark-Novels/Bookmark-Database',
     license='MIT',
     install_requires=[
         'mysqlclient',
-        'SQLAlchemy',
-        'locksmith'
-    ],
-    dependency_links=[
-        'git+git://github.com/Bookmark-Novels/Locksmith.git#egg=locksmith'
+        'SQLAlchemy'
     ]
 )
