c1b7bc3c3cfb052f60e50dbcbd3be4aaac981d26
Andrew Lee <andrew.dragon.lee@gmail.com>
2017-08-13 22:33:29 -0400

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='bookmark-database',
     description='Python package for interacting with Bookmark\'s database.',
     author='Bookmark Novels',
-    version='1.0.3',
+    version='1.0.4',
     packages=['bookmark_database'],
     url='https://github.com/Bookmark-Novels/Bookmark-Database',
     license='MIT',
